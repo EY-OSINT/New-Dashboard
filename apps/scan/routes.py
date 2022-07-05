4325dc5786fdf0f23affa5a8de6719becacf6c97
MarouaneZribi <ahmedmarouanez@gmail.com>
2022-07-05 07:38:10 -0700

    Began editting routing

--- apps/scan/routes.py
+++ apps/scan/routes.py
@@ -16,12 +16,39 @@ from flask import flash
 @blueprint.route('/scan',methods=["GET","POST"])
 @login_required
 def scan():
+    #Saving user's form input after user pushed start scan button
     if request.method=='POST':
-        target_name=request.form.get("target_name")=='target_name'
-        scan_name=request.form.get("scan_name")=='scan_name'
-        f=request.files['input']
-        f.save(os.path.join(os.getcwd(),secure_filename(f.filename)))
-     
+        if request.form['start_scan'] == 'start_scan':
+            if request.form['target_name'] == 'target_name':
+                target_name=request.form['target_name']
+            if request.form['scan_name']== 'scan_name':
+                scan_name=request.form['scan_name']
+            if request.form['github_name'] == 'github_name':
+                github_name=request.form['github_name']
+            if request.form['shodan_name'] == 'shodan_name':
+                shodan_name=request.form['shodan_name']
+            #function to be created IMPORTANT FETCH TARGET BY ... IMPORTANT 
+          #  if not bool(db_helper.fetch_scan_by_name_and_target_id(target_name,"target_name")):
+           #    db_helper.insert_new_scan(target_name,"target_name")
+            
+
+        print(target_name)
+        print(scan_name)
+        print(github_name)
+        print(scan_name)
+        if request.form['passive_scan']:
+            print("passive")
+        if request.form['active_scan']:
+            print("active")
+        if request.form['custom_scan']:
+            if request.form['Custom_Domain']:
+               if request.form.get('checkbox')='Custom_Domain':
+                f=request.files['input']
+                f.save(os.path.join(os.getcwd(),secure_filename(f.filename)))   
+            print("custom")  
+            f=request.files['input']
+            f.save(os.path.join(os.getcwd(),secure_filename(f.filename)))
+        
     return render_template('home/conf-scan.html',segment='conf-scan')
 
 """
