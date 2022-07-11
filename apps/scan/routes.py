d9452caad623139f8c5688d95295daf5cf4de871
MarouaneZribi <ahmedmarouanez@gmail.com>
2022-07-11 08:09:01 -0700

    forms.py added and html

--- apps/scan/routes.py
+++ apps/scan/routes.py
@@ -1,25 +1,27 @@
+from distutils.command.config import config
 from re import X
 from flask import render_template, redirect, request, url_for
 from fileinput      import filename
 from wtforms        import Form
 import datetime
-import apps 
-import subdomain
+from . import subdomain
 from apps.scan import blueprint
-from flask_login import login_required  
+from flask_login import login_required 
 from werkzeug.utils import secure_filename
 import os
 import apps.database  as db_helper
 from flask import flash
 from .. import Library
-import os 
-
-@blueprint.route('/scan',methods=["GET","POST"])
+from apps.scan.forms import ScanForm
+@blueprint.route('/scan_config',methods=["GET","POST"])
 @login_required
-def scan():
+def scan_config():
+    scan_form=ScanForm(request.form)
     #Saving user's form input after user pushed start scan button
     if request.method=='POST':
+        print("POST")
         if request.form['start_scan'] == 'start_scan':
+            print("yeeeet")
             if request.form['target_name'] == 'target_name':
                 target_name=request.form['target_name']
                 if not bool(db_helper.fetch_by_name("target",target_name)):
@@ -39,36 +41,39 @@ def scan():
             #function to be created IMPORTANT FETCH TARGET BY ... IMPORTANT 
           #  if not bool(db_helper.fetch_scan_by_name_and_target_id(target_name,"target_name")):
            #    db_helper.insert_new_scan(target_name,"target_name")
-        
-
             print(target_name)
             print(scan_name)
             print(github_name)
             print(scan_name)
-        if request.form['passive_scan']:
-            print("passive")
-            subdomain.Passive(target_name,scan_name)
-        if request.form['active_scan']:
-            print("active")
-            subdomain.Active(target_name,scan_name)
-        if request.form['custom_scan']:
-            print("elimiante error")
-            if request.form['Custom_Domain']:
-                if request.form.get('checkbox')!='Custom_Domain':
-                    print('ee')
-                    print('ENTER YOUR OWN LIST')
-                elif request.form.get('checkbox')=='Custom_Domain':
-                    print('whois module')
-                    print('validation')
-                f=request.files['input']
-                f.save(os.path.join(os.getcwd(),r"scans_folder/",target_name,scan_name,r"domain/upload.txt"))   
-                print("custom Domain Module NOT CHECKED file must be uploaded")
-                if request.form.get('checkbox') == 'Custom_Subdomain':
-                    subdomain.Custom(target_name,scan_name)
-                if request.form.get('checkbox')=='Custom_Directory':
-                    print('Custom_dir')
+            if request.form['passive_scan']:
+                print("passive")
+                subdomain.Passive(target_name,scan_name)
+            if request.form['active_scan']:
+                print("active")
+                subdomain.Active(target_name,scan_name)
+            if request.form['custom_scan']:
+                print("elimiante error")
+                if request.form['Custom_Domain']:
+                    if request.form.get('checkbox')!='Custom_Domain':
+                        print('ee')
+                        print('ENTER YOUR OWN LIST')
+                    elif request.form.get('checkbox')=='Custom_Domain':
+                        print('whois module')
+                        print('validation')
+                    f=request.files['input']
+                    f.save(os.path.join(os.getcwd(),r"scans_folder/",target_name,scan_name,r"domain/upload.txt"))   
+                    print("custom Domain Module NOT CHECKED file must be uploaded")
+                    if request.form.get('checkbox') == 'Custom_Subdomain':
+                        subdomain.Custom(target_name,scan_name)
+                    if request.form.get('checkbox')=='Custom_Directory':
+                        print('Custom_dir')
+        return render_template(url_for('scan_blueprint.all_scans'))
     return render_template('home/conf-scan.html',segment='conf-scan')
+@blueprint.route('/all_scans',methods=["GET","POST"])
+@login_required
+def all_scans():
 
+    return
 """
    scans= db_helper.fetch_scan_by_name("target_name") 
    if request.method == 'POST' and not(str(request.get_data()) == "b'add='") :
